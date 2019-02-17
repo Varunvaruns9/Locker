032fcf6f2925fb22cd20f58bca480c180901f351
varunvaruns9 <varunsinghs2021@gmail.com>
2019-02-17 14:22:03 +0530

    Add views file.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics
+from .models import Locker, Log
+from .serializers import LockerSerializer, LogSerializer
