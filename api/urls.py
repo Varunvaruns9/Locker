6a7b22e0b181e13a7ccdc71b9d7425f9d8f7cb47
varunvaruns9 <varunsinghs2021@gmail.com>
2019-02-17 15:29:03 +0530

    Add urls.

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-	
+	path('locker/<int:locker_id>/camera', views.CameraView),
+	path('locker/<int:locker_id>/status', views.StatusView),
+	path('locker/<int:locker_id>/open', views.OpenView),
 ]
