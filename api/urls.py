f3a12a197b531efb2b2fe08fa22d8406bc932fda
varunvaruns9 <varunsinghs2021@gmail.com>
2019-02-18 00:03:44 +0530

    Add authentication.

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+	path('login', views.login),
 	path('locker', views.AllLockers.as_view()),
 	path('locker/<int:locker_id>/camera', views.CameraView.as_view()),
 	path('locker/<int:locker_id>/status', views.StatusView.as_view()),
--- api/views.py
+++ api/views.py
@@ -1,4 +1,9 @@
 from django.http import Http404
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -71,3 +76,21 @@ class OpenView(APIView):
 			serializer.save()
 			return Response(serializer.data)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({'error': 'Invalid Credentials'},
+                        status=status.HTTP_404_NOT_FOUND)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key},
+                    status=status.HTTP_200_OK)
