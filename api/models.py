7e5ce06fd1017c184898608ff0f32300eeb662cb
varunvaruns9 <varunsinghs2021@gmail.com>
2019-02-17 13:50:48 +0530

    Add database models and urls file.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Locker(models.Model):
+	# True indicates locker is currently open
+	open_status = models.BooleanField(default=False)
+	# True indicates locker is accessible
+	accessible = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.pk
+
+
+class Log(models.Model):
+	locker = models.ForeignKey('Locker', on_delete=models.CASCADE)
+	time = models.DateTimeField(default=timezone.now)
+	pics = models.URLField(max_length=512)
+
+	def __str__(self):
+		return self.Locker.pk + ' - ' + self.time
